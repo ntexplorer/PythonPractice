a874d4fef9d7571cdfbb81b14f17be7f665bb2c1
ntexplorer <ntexplorer@qq.com>
2019-10-07 14:27:19 +0100

    py practice Chap3 updated

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,16 @@
 print(3.0/2.0)
 print("hello, world!")
+bicycle = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycle)
+print(bicycle[2].upper())
+message = "My first bicycle is a " + bicycle[3].title() + "."
+print(message)
+bicycle.append("ducati")
+print(bicycle)
+bicycle.insert(1,"honda")
+print(bicycle)
+del bicycle[1]
+print(bicycle)
+z = 1
+z = z + 1
+print(z)
