66238f8aebb797f650fadea856f7ae70a72532ff
ntexplorer <ntexplorer@qq.com>
2019-10-07 21:49:15 +0100

    Chap4 Prac Updated

---
+++ Chapter3_Prac/3-8_10.py
@@ -0,0 +1,17 @@
+place = ["Guizhou", "Yunnan", "Beijing"]
+print(place)
+sorted(place)
+print(place)
+
+sorted(place, reverse=True)
+place.reverse()
+print(place)
+place.reverse()
+print(place)
+
+place.sort()
+print(place)
+place.sort(reverse=True)
+print(place)
+
+print(len(place))
