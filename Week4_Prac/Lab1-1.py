0499ff4c784a8ecddeb3df0cd9da2c5374f4f35f
ntexplorer <ntexplorer@qq.com>
2019-10-23 21:58:11 +0100

    Week4 Prac updated, not finished.

---
+++ Week4_Prac/Lab1-1.py
@@ -0,0 +1,4 @@
+a = [[1,2,3,4],[5,6,7,8],['a','b','c','d']]
+for i in a:
+    for n in i:
+        print(n)
---
+++ Week4_Prac/Lab2-1.py
@@ -0,0 +1,16 @@
+import os
+
+d = {}
+
+path = "./"
+
+with os.scandir(path) as it:
+    for entry in it:
+        if xxxx:
+            xxxx
+
+            #  entry.is_file()
+            #  entry.name
+            #  entry.stat()
+            #  import datetime
+            #  datetime.datetim.fromtimestamp()
