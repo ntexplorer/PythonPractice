bf6404ad9684f5684a88088203adcf53ffac7deb
ntexplorer <ntexplorer@qq.com>
2019-11-04 12:49:45 +0000

    Week5 homework finished

--- Week5_Prac/game/playguessnumber.py
+++ Week5_Prac/game/playguessnumber.py
@@ -3,4 +3,6 @@ import guess_number_game
 
 if __name__ == '__main__':
         guessnumber = guess_number_game.GuessNumberGame(5)
-        #To be completed
+        guessnumber.greet()
+        guessnumber.play()
+        guessnumber.epilogue()
